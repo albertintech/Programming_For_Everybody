91f6f51d992938ca78fdb81f199f3b4e305a4ec3
Albert Ramos <albertramos.dev@gmail.com>
2021-12-22 15:21:32 -0700

    Add solution

--- fun_functions.py
+++ fun_functions.py
@@ -1,10 +1,17 @@
-def happy():
-    print("Happy! " * 2)
-    print("Joy! " * 2)
+# 4.6 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours. Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program(the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly. Do not name your variable sum or use the sum() function.
 
-happy()
-print("Time for Spam and Eggs!")
-happy()
+def computepay(h, r):
+    if h <= 40:
+        result = h * r
+    else:
+        reg_pay = 40 * r
+        ot_pay = (h - 40) * (r * 1.5)
+        result = reg_pay + ot_pay
+    return result
 
-big = max("ZyyWpqrSa SQPR")
-print(big)
+hrs_str = input("Enter Hours:")
+rph_str = input("Enter Rate per Hour:")
+hrs = float(hrs_str)
+rph = float(rph_str)
+p = computepay(hrs, rph)
+print("Pay", p)
