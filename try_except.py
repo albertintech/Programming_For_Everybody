222125878b513a2a8680ec7bd09d8465a1ee8303
Albert Ramos <albertramos.dev@gmail.com>
2021-12-22 06:18:17 -0700

    Update file

--- try_except.py
+++ try_except.py
@@ -8,4 +8,17 @@ except:
     new_int = -1
     print("Try block didn't work. Executed except block instead.")
 
-print('First result', new_int)
+print('Value of new_int:', new_int)
+
+# Let's try again
+a_str = '123'
+print(f"Initial string value is: {a_str}")
+
+try:
+    new_int = int(a_str)
+    print("It worked on the try block.")
+except:
+    new_int = -1
+    print("Try block didn't work. Executed except block instead.")
+
+print('Value of new_int:', new_int)
