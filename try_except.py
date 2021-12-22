07c4da2c848fa046f8d057286b7f2fabc560913e
Albert Ramos <albertramos.dev@gmail.com>
2021-12-22 06:12:46 -0700

    Add file

---
+++ try_except.py
@@ -0,0 +1,11 @@
+a_str = 'Hi Bob'
+print(f"Initial string value is: {a_str}")
+
+try:
+    new_int = int(a_str)
+    print("It worked on the try block.")
+except:
+    new_int = -1
+    print("Try block didn't work. Executed except block instead.")
+
+print('First result', new_int)
