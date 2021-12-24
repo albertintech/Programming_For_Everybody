c2853316d6429ba26aff61f35e0acc47d3e4ced3
Albert Ramos <albertramos.dev@gmail.com>
2021-12-23 20:38:41 -0700

    Add file

--- definite_loops.py
+++ definite_loops.py
@@ -14,3 +14,20 @@ for i in num_list:
     print(f"The largest number so far is {largest_num}")
 print(f"{largest_num} is the largest number in the list.")
 
+num_list = [1003, 99, 12, 9, 2, 1, -42]
+smallest_num = max(num_list) + 1
+for i in num_list:
+    if i < smallest_num:
+        smallest_num = i
+    print(f"The smallest number so far is {smallest_num}")
+print(f"{smallest_num} is the smallest number in the list.")
+
+num_list = [1003, 99, 12, 9, 2, 1, -42]
+smallest_num = None
+for i in num_list:
+    if smallest_num is None:
+        smallest_num = i
+    elif i < smallest_num:
+        smallest_num = i
+    print(f"The smallest number so far is {smallest_num}")
+print(f"{smallest_num} is the smallest number in the list.")
