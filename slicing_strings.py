82222067e8da9a550f15d03b56ea62971754eb25
Albert Ramos <albertramos.dev@gmail.com>
2021-12-25 13:30:26 -0700

    Update file

--- slicing_strings.py
+++ slicing_strings.py
@@ -1,14 +1,19 @@
 # pylint: disable=invalid-name, missing-docstring
 
-s = "Monty Python"
-print(s[:5])
-print(s[6:])
-print(s[-6:])
-print(s[::-1])
-print()
+# s = "Monty Python"
+# print(s[:5])
+# print(s[6:])
+# print(s[-6:])
+# print(s[::-1])
+# print()
 
 text = "X-DSPAM-Confidence:    0.8475"
 col_pos = text.find(":")
 raw_number = text[col_pos + 1:]
-number = raw_number.strip()
+number_str = raw_number.strip()
+print(number_str)
+print(type(number_str))
+print()
+number = float(number_str)
 print(number)
+print(type(number))
