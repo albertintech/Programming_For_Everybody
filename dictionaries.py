f75494e6407e27e5c7cc7f1ba0ad7be61240aab1
Albert Ramos <albertramos.dev@gmail.com>
2021-12-28 08:01:34 -0700

    Add file

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+# pylint: disable=invalid-name, missing-docstring, line-too-long, unspecified-encoding
+
+purse = dict()
+purse['money'] = 40.00
+purse['candies'] = 4
+purse['earphones'] = 1
+print(purse)
