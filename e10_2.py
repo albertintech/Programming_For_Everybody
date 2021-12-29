2c57537d3d55c2603de4b5e3cce2170d19b651bd
Albert Ramos <albertramos.dev@gmail.com>
2021-12-28 17:02:42 -0700

    Edit docstring

--- e10_2.py
+++ e10_2.py
@@ -1,25 +1,28 @@
-# 10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+"""
+10.2
+Write a program to read through the mbox-short.txt.
+Isolate the following line types:
+"From stephen.marquard@uct.ac.za Sat Jan  5 09: 14: 16 2008"
+Then, figure out the distribution of emails by hour of the day.
+Print out the counts, sorted by ascending hour as shown below.
 
-# From stephen.marquard@uct.ac.za Sat Jan  5 09: 14: 16 2008
-
-# Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
-
-# Desired Output
-# 04 3
-# 06 1
-# 07 1
-# 09 2
-# 10 3
-# 11 6
-# 14 1
-# 15 2
-# 16 4
-# 17 2
-# 18 1
-# 19 1
+Desired Output
+04 3
+06 1
+07 1
+09 2
+10 3
+11 6
+14 1
+15 2
+16 4
+17 2
+18 1
+19 1
+"""
 
 fname = input("Enter file name: ")
-fh = open(fname)
+fh = open(fname, encoding='UTF-8')
 
 times = list()
 hours = list()
