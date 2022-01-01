2f90318ac135bbb9a1d290f60ed2a6d72bc8fe6d
Albert Ramos <albertramos.dev@gmail.com>
2022-01-01 07:40:34 -0700

    Add comments

--- web_data/socket.py
+++ web_data/socket.py
@@ -2,8 +2,11 @@
 
 """Demonstration of Python's built-in support for TCP Sockets"""
 
-
+# Get the library
 import socket
 
+# Create the socket object, the 'doorway'
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))  # Host and port number
+
+# Use the connect method on socket object to, well, connect!
+mysock.connect(('data.pr4e.org', 80))
