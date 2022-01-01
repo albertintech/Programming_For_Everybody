fdb86ea57b26cd5f26cef9b438c1a7cb1f9cf0ec
Albert Ramos <albertramos.dev@gmail.com>
2022-01-01 09:28:08 -0700

    Update files

--- web_data/scratch.py
+++
@@ -1,9 +0,0 @@
-import re
-
-x = 'From: Using the : character'
-y = re.findall('^F.+:', x)
-print(y)
-
-a = "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
-b = re.findall('\S+?@\S+', a)
-print(b)
